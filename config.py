3f7ce21234d56b5ba8f92f9b5e371345e4ceb208
Johns Baby <johnsbabymemana@gmail.com>
2022-12-13 18:52:06 +0530

    sec commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database.c3eu1fpa4ajz.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "welai"
+# custombucket = "add-employee-2022"
+customregion = "us-east-1"
